110135c08eaa98e78974abedf8b0eeab9a8a465b
Dźwiedź <kg67199@gmail.com>
2020-08-19 15:15:28 +0200

    Initial commit

---
+++ src/main/java/dzwdz/cursed_enchanting_table/EntryPoint.java
@@ -0,0 +1,10 @@
+package dzwdz.cursed_enchanting_table;
+
+import net.fabricmc.api.ModInitializer;
+
+public class EntryPoint implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
